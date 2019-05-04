7a9d44326a8ea8df574ecccd8353208a3ac825b8
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-05-04 22:08:10 +0200

    add database and tests

---
+++ README.rst
@@ -0,0 +1,16 @@
+=============
+Node Blog App
+=============
+
+Learning Node - Practical Node.js by Azat Mardan
+
+To see ``node-blog-app`` in action clone the repository and run it locally::
+
+    $ git clone https://github.com/trco/node-blog-app.git
+    $ cd node-blog-app
+    $ npm install
+    $ node app.js
+
+Run tests with::
+
+    $ npm test
--- test/index.js
+++ test/index.js
@@ -3,12 +3,18 @@ var shutdown = require('../app').shutdown;
 var port = require('../app').port;
 var superagent = require('superagent');
 var expect = require('expect.js');
+var seedArticles = require('../db/articles.json');
 
+
+// Test server boot, shutdown & homepage GET
 describe('server', function () {
+
   before(function () {
     boot();
   });
+
   describe('homepage', function () {
+    // Test GET request
     it('should respond to GET', function (done) {
       superagent
         .get('http://127.0.0.1:' + port)
@@ -17,8 +23,54 @@ describe('server', function () {
           done();
         });
     });
-  });
+    // Test content of homepage
+    it('should contain posts', function (done) {
+      // GET request to homepage
+      superagent
+        .get('http://127.0.0.1:' + port)
+        .end(function (res) {
+          // Check if received response contains articles present in seedArticles
+          seedArticles.forEach(function (item, index, list) {
+            if (item.published) {
+              expect(res.text).to.contain(
+                '<h2><a href="/articles/' + item.slug + '">' + item.title
+              );
+            } else {
+              expect(res.text).not.to.contain(
+                '<h2><a href="/articles/' + item.slug + '">' + item.title
+              );
+            }
+          });
+          done();
+        });
+    });
+  }); // End 'homepage' test suite
+
+  describe('article page', function () {
+    it('should display text', function (done) {
+      var n = seedArticles.length;
+      // GET request for each article
+      seedArticles.forEach(function (item, index, list) {
+        superagent
+          .get('http://127.0.0.1:' + port + '/articles/' + seedArticles[index].slug)
+          .end(function(res) {
+            if (item.published) {
+              expect(res.text).to.contain(
+                seedArticles[index].text
+              );
+            } else {
+              expect(res.status).to.be(401);
+            }
+            if (index + 1 === n) {
+              done();
+            }
+          });
+      });
+    });
+  }); // End 'article page' test suite
+
   after(function () {
     shutdown();
   });
-});
+
+}); // End 'server' test suite
